50379b681e5970189b7d8a7e1535f3e1c56c2dc8
潘梦怡 <2179006647@qq.com>
2022-11-24 15:00:44 +0800

    柱状图第一次提交

--- js/1.js
+++ js/1.js
@@ -83,4 +83,117 @@
 })();
 
 
+// 用户统计图
+(function () {
+    var myChart = echarts.init(document.getElementsByClassName('stat')[0]);
+
+
+    var option = {
+        color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [
+                { offset: 0, color: '#00fffb' }, // 0 起始颜色
+                { offset: 1, color: '#0061ce' }  // 1 结束颜色
+            ],
+            global: false
+
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+
+                type: 'none'
+            }
+        },
+        grid: {
+            top: '4%',
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true,
+            show: true,
+            borderColor: 'rgba(0, 240, 255, 0.3)'
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['郑州', '广州', '北京', '深圳', '合肥', '', '......', '', '杭州', '厦门', '济南', '成都', '重庆'],
+                axisTick: {
+                    alignWithLabel: false
+                },
+                axisLabel: {
+                    color: "#71f2fb"
+                },
+                axisLine: {
+                    lineStyle: {
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                }
+            }
+
+        ],
+        yAxis: [
+            {
+                type: 'value',
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    color: "#71f2fb"
+                },
+                splitLine: {
+                    lineStyle: {
+                        // 使用深浅的间隔色
+                        color: 'rgba(0, 240, 255, 0.3)'
+                    }
+                }
+            }
+        ],
+
+        series: [
+            {
+                name: 'Direct',
+                type: 'bar',
+                barWidth: '60%',
+                data: [2100, 1900, 1700, 1560, 1400, {
+                    value: 1200,
+                    itemStyle: {
+                        color: '#254065'
+                    }
+                }, {
+                        value: 1200,
+                        itemStyle: {
+                            color: '#254065'
+                        }
+                    }, {
+                        value: 1200,
+                        itemStyle: {
+                            color: '#254065'
+                        }
+                    }, 900, 750, 600, 480, 240]
+            },
+
+        ]
+    };
+
+
+
+    myChart.setOption(option);
+
+
+    window.addEventListener('load', function () {
+        myChart.resize();
+    });
+
+    window.addEventListener('resize', function () {
+        myChart.resize();
+    })
+})();
+
+
+
 
